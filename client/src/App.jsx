c5b74ff69cfde3d4b8c69ad83dd863342088fd95
SW33TSTUFF <simaakniyaz@gmail.com>
2024-08-23 18:16:57 +0530

    Added dark mode and fixed header

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,28 +1,51 @@
-import './App.css';
-import Navbar from './Components/Navbar/Navbar';
-import {BrowserRouter,Routes,Route} from 'react-router-dom' 
-import Shop from './Pages/Shop'
-import ShopCategory from './Pages/ShopCategory'
-import Product from './Pages/Product'
-import Cart from './Pages/Cart'
-import LoginSignUp from './Pages/LoginSignUp'
+import "./styles.css";
+import Navbar from "./Components/Navbar/Navbar";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Shop from "./Pages/Shop";
+import ShopCategory from "./Pages/ShopCategory";
+import Product from "./Pages/Product";
+import Cart from "./Pages/Cart";
+import LoginSignUp from "./Pages/LoginSignUp";
+
+import { useState, useEffect } from "react";
 
 function App() {
+  const [theme, setTheme] = useState("light");
+
+  useEffect(() => {
+    if (window.matchMedia("(prefers-color-scheme: dark)").matches) {
+      setTheme("dark");
+    } else {
+      setTheme("light");
+    }
+  }, []);
+
+  useEffect(() => {
+    if (theme === "dark") {
+      document.documentElement.classList.add("dark");
+    } else {
+      document.documentElement.classList.remove("dark");
+    }
+  }, [theme]);
+
+  const handleThemeSwitch = () => {
+    setTheme((prevTheme) => (prevTheme === "dark" ? "light" : "dark"));
+  };
+
   return (
     <div>
-        <Navbar />
-        <Routes>
-          <Route path='/' element = {<Shop />} />
-          <Route path='/mens' element = {<ShopCategory category="mens"/>} />
-          <Route path='/women' element = {<ShopCategory category="women"/>} />
-          <Route path='/kids' element = {<ShopCategory category="kids"/>} />
-          <Route path='product' element={<Product/>}>
-            <Route path='/product/:productId' element={<Product />}/>
-          </Route>
-          <Route path='/cart' element = {<Cart />} />
-          <Route path='/login' element = {<LoginSignUp />} />
-        </ Routes>
-
+      <Navbar theme={theme} onThemeSwitch={handleThemeSwitch} />
+      <Routes>
+        <Route path="/" element={<Shop />} />
+        <Route path="/mens" element={<ShopCategory category="mens" />} />
+        <Route path="/women" element={<ShopCategory category="women" />} />
+        <Route path="/kids" element={<ShopCategory category="kids" />} />
+        <Route path="product" element={<Product />}>
+          <Route path="/product/:productId" element={<Product />} />
+        </Route>
+        <Route path="/cart" element={<Cart />} />
+        <Route path="/login" element={<LoginSignUp />} />
+      </Routes>
     </div>
   );
 }
