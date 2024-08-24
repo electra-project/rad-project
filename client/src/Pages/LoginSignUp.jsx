42481f67338cf43a2a1810c5e154a9b26f08a546
SW33TSTUFF <simaakniyaz@gmail.com>
2024-08-24 20:43:52 +0530

    Added mobile responsiveness to Products + Categories

--- client/src/Pages/LoginSignUp.jsx
+++ client/src/Pages/LoginSignUp.jsx
@@ -1,25 +1,27 @@
-import React from 'react'
-import './CSS/LoginSignUp.css'
+import React from "react";
+import "./CSS/LoginSignUp.css";
 
 const LoginSignUp = () => {
   return (
-    <div className='loginsignup'>
+    <div className="loginsignup">
       <div className="loginsignup-container">
         <h1> Sign Up</h1>
         <div className="loginsignup-fields">
-          <input type="text" placeholder='Your Name'/>
-          <input type="email" placeholder='Email address'/>
-          <input type="password" placeholder='Password'/>
+          <input type="text" placeholder="Your Name" />
+          <input type="email" placeholder="Email address" />
+          <input type="password" placeholder="Password" />
         </div>
         <button> Continue </button>
-        <p className="loginsignup-login"> Already have an account?<span>Login here</span></p>
+        <p className="loginsignup-login">
+          Already have an account?<span>Login here</span>
+        </p>
         <div className="loginsignup-agree">
           <input type="ckeckbox" name="" id="" />
-          <p> By Continuing, i agree to the terms of use & privacy policy</p>
+          <p> By Continuing, I agree to the terms of use & privacy policy</p>
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default LoginSignUp
+export default LoginSignUp;
