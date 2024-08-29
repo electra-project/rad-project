5956fc2481242b33976a0fcd422154561c08b04b
Abdullaaurad <abdullaaurad@gmail.com>
2024-08-30 01:22:13 +0530

    create Admin dashboard

---
+++ Frontend/src/Components/NewCollections/NewCollections.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+import "./NewCollection.css";
+import new_collection from "../Assets/new_collections";
+import Item from "../Items/Items";
+
+const NewCollections = () => {
+  return (
+    <div className="new-collection">
+      <h1> Latest Featured Products </h1>
+      <hr />
+      <div className="collections">
+        {new_collection.map((item, i) => {
+          return (
+            <Item
+              key={i}
+              id={item.id}
+              name={item.name}
+              image={item.image}
+              new_price={item.new_price}
+              old_price={item.old_item}
+            />
+          );
+        })}
+      </div>
+    </div>
+  );
+};
+
+export default NewCollections;
